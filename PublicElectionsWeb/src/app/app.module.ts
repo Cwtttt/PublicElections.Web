10745d84209b26efbd45e21518f8d8b3cc41bd26
mDevelopers <38528313+Cwtttt@users.noreply.github.com>
2021-03-13 17:38:12 +0100

    basic components & routing

--- PublicElectionsWeb/src/app/app.module.ts
+++ PublicElectionsWeb/src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RegisterComponent } from './components/register/register.component';
+import { HeaderComponent } from './components/header/header.component'; 
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    RegisterComponent,
+    HeaderComponent,
+    DashboardComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
-})
+}) 
 export class AppModule { }
